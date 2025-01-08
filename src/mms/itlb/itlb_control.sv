c42b9e354f4dddee001b63aa0ab64b19cb6a58bd
archX1597 <1047792557@qq.com>
2025-01-08 16:32:25 +0800

    update TLB control

--- src/mms/itlb/itlb_control.sv
+++ src/mms/itlb/itlb_control.sv
@@ -15,12 +15,37 @@ module itlb_control (
     pte_t  pte;
     logic tlb_miss;
     logic tlb_hit ;
+    logic tlb_access;
+    logic rsw_valid;
+    logic vir_mem_en;
+    logic sv32_en;
 
+
+    assign tlb_access  = rd_access_i | wr_access_i;
     assign satp = satp_i;
     assign pte = pte_i  ;
-    assign tlb_miss   = ~tag_hit_i | ~pte.read & pte.write | ~pte.xcute | (satp.mode == 2'b00) ;
-    assign tlb_hit    = ~tlb_miss ;
-    assign tlb_miss_o = tlb_miss;
-    assign tlb_hit_o  = tlb_hit ;  
+
+    assign vir_mem_en = (satp.mode == 2'b00);
+    assign sv32_en   = (satp.mode == 2'b01);
+    assign rsw_valid = (pte.rsw == 2'b00);
+    assign rd_valid  = pte.read;
+    assign wr_valid  = pte.write;
+    assign xc_valid  = pte.xcute;
+
+
+    //exception:
+    //1. page fault
+    //2. page table entry invalid
+    //3. page table entry not found
+    //4. page table entry not valid
+    //5. page table entry not readable
+    //6. page table entry not writable
+    //7. page table entry not executable
+
+
+
+
+
+
 
 endmodule
