80f8dc6d6c12fe9e44fdd8b92b87ea1f38b68f4a
archX1597 <1047792557@qq.com>
2025-01-21 23:32:39 +0800

    Divide the pc_Gen and Redirector

--- src/core/ifu/pcGen.sv
+++ src/core/ifu/pcGen.sv
@@ -5,16 +5,9 @@ module pcGen
         input  logic                   i_rst,
         input  logic                   i_clk,
         input  logic                   i_stall,
-        input  logic [`MXLEN - 1 : 0]  i_if0_pcGen_npc,
-        input  logic [`MXLEN - 1 : 0]  i_if1_pcGen_npc,
-        input  logic [`MXLEN - 1 : 0]  i_if2_pcGen_npc,
-        input  logic [`MXLEN - 1 : 0]  i_exu_pcGen_npc,
-        input  logic                   i_pcIf0_RedirectArb_valid,
-        input  logic                   i_pcIf1_RedirectArb_valid,
-        input  logic                   i_pcIf2_RedirectArb_valid,
-        input  logic                   i_pcExu_RedirectArb_valid,
-        output logic [`MXLEN - 1 : 0]  o_pcGen_npc,
-        output logic [`MXLEN - 1 : 0]  o_pcGen_cPc,
+        input  logic [`MXLEN - 1 : 0]  i_pcRedirect_pcGen_pc,
+        input  logic                   i_pcRedirect_pcGen_pc_valid,
+        output logic [`MXLEN - 1 : 0]  o_pcGen_cPc,o_pcGen_npc,
         output logic                   o_pcGen_npcFetch_valid
     );
 
@@ -25,21 +18,18 @@ module pcGen
     assign boot_pc  = `BOOT_PC;
     assign stall_pc = pcGen_pc_q;
     assign seq_pc = (pcGen_pc_q[3:2] == 2'b00) ? pcGen_pc_q + 16:
-           (pcGen_pc_q[3:2] == 2'b01) ? pcGen_pc_q + 12:
-           (pcGen_pc_q[3:2] == 2'b10) ? pcGen_pc_q + 8:
-           (pcGen_pc_q[3:2] == 2'b11) ? pcGen_pc_q + 4:
-           pcGen_pc_q;
+        (pcGen_pc_q[3:2] == 2'b01) ? pcGen_pc_q + 12:
+        (pcGen_pc_q[3:2] == 2'b10) ? pcGen_pc_q + 8:
+        (pcGen_pc_q[3:2] == 2'b11) ? pcGen_pc_q + 4:
+        pcGen_pc_q;
 
-    //Redirect:priority :  exu > if2 > if1 > if0
-    assign redirect_pc = (i_pcExu_RedirectArb_valid) ? i_exu_pcGen_npc:
-        (i_pcIf2_RedirectArb_valid) ? i_if2_pcGen_npc:
-        (i_pcIf1_RedirectArb_valid) ? i_if1_pcGen_npc:
-        (i_pcIf0_RedirectArb_valid) ? i_if0_pcGen_npc:
-        seq_pc;
-    //next pc priority : rst>stall > redirect
+    assign redirect_pc = i_pcRedirect_pcGen_pc ;
+
+    //next pc priority : i_rst > i_stall > i_redirect_pcGen_valid
     assign pcGen_pc_d  = i_rst   ? boot_pc:
-                         i_stall ? stall_pc:
-                         redirect_pc;
+        i_stall ? stall_pc:
+        i_pcRedirect_pcGen_pc_valid ? redirect_pc:
+        seq_pc;
 
 
     always_ff @(posedge i_clk) begin:pcGen_ff
--- src/core/ifu/pcRedirect.sv
+++ src/core/ifu/pcRedirect.sv
@@ -1,39 +1,63 @@
-import risXv_macro::*;
+import risXv_macro:
+:
+*;
 `define REDIR_SRC_NUM 4
 //This Component is used to arbitrate the pcRedirect request from different modules
 //The priority is exu > if2 > if1 > if0
-//The output is the valid signal of each 
+//The output is the valid signal of each
 //Combination Circuit
 module pcRedirect
     (
+
+        input  logic  [`MXLEN - 1 : 0] i_if0_pcRedirect_npc,
+        input  logic  [`MXLEN - 1 : 0] i_if1_pcRedirect_npc,
+        input  logic  [`MXLEN - 1 : 0] i_if2_pcRedirect_npc,
+        input  logic  [`MXLEN - 1 : 0] i_exu_pcRedirect_npc,
+        //valid signal of each module
         input  logic                  i_if0_pcRedirect_npc_valid,
         input  logic                  i_if1_pcRedirect_npc_valid,
         input  logic                  i_if2_pcRedirect_npc_valid,
         input  logic                  i_exu_pcRedirect_npc_valid,
-        output logic                  o_pcIf0_RedirectArb_valid,
-        output logic                  o_pcIf1_RedirectArb_valid,
-        output logic                  o_pcIf2_RedirectArb_valid,
-        output logic                  o_pcExu_RedirectArb_valid
+        output logic                  o_pcRedirect_npcGen_npc,
+        output logic                  o_pcRedirect_npcGen_redirect_valid
     );
 
-        //priority : exu > if2 > if1 > if0
-        logic [`REDIR_SRC_NUM - 1 : 0]   Redirect_req,pcRedirectArb_valid;
-        assign Redirect_req = {i_exu_pcRedirect_npc_valid,i_if2_pcRedirect_npc_valid,i_if1_pcRedirect_npc_valid,i_if0_pcRedirect_npc_valid};
-        assign o_pcIf0_RedirectArb_valid = pcRedirectArb_valid[0];
-        assign o_pcIf1_RedirectArb_valid = pcRedirectArb_valid[1];
-        assign o_pcIf2_RedirectArb_valid = pcRedirectArb_valid[2];
-        assign o_pcExu_RedirectArb_valid = pcRedirectArb_valid[3];
-
-        always_comb begin
-            unique case(Redirect_req)
-                4'b1???: pcRedirectArb_valid = 4'b1000;
-                4'b01??: pcRedirectArb_valid = 4'b0100;
-                4'b001?: pcRedirectArb_valid = 4'b0010;
-                4'b0001: pcRedirectArb_valid = 4'b0001;
-                default: pcRedirectArb_valid = 4'b0000;
-            endcase
+    //priority : exu > if2 > if1 > if
+    always_comb begin
+        if(i_exu_pcRedirect_npc_valid) begin
+            o_pcRedirect_npcGen_npc = i_exu_pcRedirect_npc;
         end
-        
+        else if(i_if2_pcRedirect_npc_valid) begin
+            o_pcRedirect_npcGen_npc = i_if2_pcRedirect_npc;
+        end
+        else if(i_if1_pcRedirect_npc_valid) begin
+            o_pcRedirect_npcGen_npc = i_if1_pcRedirect_npc;
+        end
+        else if(i_if0_pcRedirect_npc_valid) begin
+            o_pcRedirect_npcGen_npc = i_if0_pcRedirect_npc;
+        end
+        else begin
+            o_pcRedirect_npcGen_npc = o_pcRedirect_npcGen_npc;
+        end
+    end
+
+    assign o_pcRedirect_npcGen_redirect_valid = i_exu_pcRedirect_npc_valid |
+                                                i_if2_pcRedirect_npc_valid |
+                                                i_if1_pcRedirect_npc_valid |
+                                                i_if0_pcRedirect_npc_valid;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 endmodule
